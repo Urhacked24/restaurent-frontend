dfa1c4118543d832c74abfac8cca3796fb24cfde
Urhacked24 <mekchandvikas@gmail.com>
2025-09-02 17:24:56 +0530

    backend link

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -30,7 +30,7 @@ const Reservation = () => {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                withCredentials: true
+                // withCredentials: true
             });
             toast.success(data.message);
             setFirstName("");
